a6a8f8c5d3f9b5741843a36ccdbe5b0ac169dc72
Icarus <wizardicarus@gmail.com>
2020-02-12 17:11:42 +0800

    update test_samples

--- test_samples.py
+++ test_samples.py
@@ -7,7 +7,87 @@ from tqdm import tqdm
 
 from model import PixelwiseRegression
 import datasets
-from utils import load_model, recover_uvd, select_gpus, draw_skeleton_torch
+from utils import load_model, recover_uvd, select_gpus
+
+def draw_skeleton(_img, joints, *, output_size=512, rP = 8, linewidth = 4, draw=True, skeleton_mode=0):
+    fig, axes = plt.subplots(figsize=(4, 4))
+    if joints.shape[0] == 14:
+        Index = [13, 1, 0]
+        Mid = [13, 3, 2]
+        Ring = [13, 5, 4]
+        Small = [13, 7, 6]
+        Thumb = [13, 10, 9, 8]
+        PALM = [11, 13, 12]
+        config = [Thumb, Index, Mid, Ring, Small, PALM]
+        img = cv2.resize(_img, (output_size, output_size))
+        img3D = np.zeros((img.shape[0], img.shape[1], 3))
+        for i in range(3):
+            img3D[:, :, i] = img
+        is_hand = img3D != 0
+        img3D = img3D / np.max(img3D)
+        # img3D = img3D * 0.5 + 0.25
+        img3D = 1 - img3D
+        img3D[is_hand] *= 0.5
+        joints = joints * (img.shape[0] - 1) + np.array([img.shape[0] // 2, img.shape[0] // 2])
+        _joint = [(int(joints[i][0]), int(joints[i][1])) for i in range(joints.shape[0])]
+        colors = [(1, 0, 0), (0.5, 0.5, 0), (0, 1, 0), (0, 0.5, 0.5), (0, 0, 1), (0.5, 0.5, 0.5)]
+        for i in range(6):
+            for index in config[i]:
+                cv2.circle(img3D, _joint[index], rP, colors[i], -1)
+            for j in range(len(config[i]) - 1):
+                cv2.line(img3D, _joint[config[i][j]], _joint[config[i][j+1]], colors[i], linewidth)
+        if draw:
+            axes.imshow(img3D)
+            axes.axis("off")
+            plt.show()
+        else:
+            return img3D
+    else:
+        if joints.shape[0] == 21:
+            if skeleton_mode == 0:
+                Index = [0, 1, 2, 3, 4]
+                Mid = [0, 5, 6, 7, 8]
+                Ring = [0, 9, 10, 11, 12]
+                Small = [0, 13, 14, 15, 16]
+                Thumb = [0, 17, 18, 19, 20]
+                config = [Thumb, Index, Mid, Ring, Small]
+            elif skeleton_mode == 1:
+                Index = [0, 2, 9, 10, 11]
+                Mid = [0, 3, 12, 13, 14]
+                Ring = [0, 4, 15, 16, 17]
+                Small = [0, 5, 18, 19, 20]
+                Thumb = [0, 1, 6, 7, 8]
+                config = [Thumb, Index, Mid, Ring, Small]
+        elif joints.shape[0] == 16:
+            Index = [0, 4, 5, 6]
+            Mid = [0, 7, 8, 9]
+            Ring = [0, 10, 11, 12]
+            Small = [0, 13, 14, 15]
+            Thumb = [0, 1, 2, 3]
+            config = [Thumb, Index, Mid, Ring, Small]
+        img = cv2.resize(_img, (output_size, output_size))
+        img3D = np.zeros((img.shape[0], img.shape[1], 3))
+        for i in range(3):
+            img3D[:, :, i] = img
+        is_hand = img3D != 0
+        img3D = img3D / np.max(img3D)
+        # img3D = img3D * 0.5 + 0.25
+        img3D = 1 - img3D
+        img3D[is_hand] *= 0.5
+        joints = joints * (img.shape[0] - 1) + np.array([img.shape[0] // 2, img.shape[0] // 2])
+        _joint = [(int(joints[i][0]), int(joints[i][1])) for i in range(joints.shape[0])]
+        colors = [(1, 0, 0), (0.5, 0.5, 0), (0, 1, 0), (0, 0.5, 0.5), (0, 0, 1)]
+        for i in range(5):
+            for index in config[i]:
+                cv2.circle(img3D, _joint[index], rP, colors[i], -1)
+            for j in range(len(config[i]) - 1):
+                cv2.line(img3D, _joint[config[i][j]], _joint[config[i][j+1]], colors[i], linewidth)
+        if draw:
+            axes.imshow(img3D)
+            axes.axis("off")
+            plt.show()
+        else:
+            return img3D
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -27,7 +107,7 @@ if __name__ == '__main__':
     parser.add_argument('--heatmap_method', type=str, default='softmax', help='choose from softmax and sumz')
 
     parser.add_argument('--gpu_id', type=str, default='0')
-    parser.add_argument("--num_workers", type=int, default=9999)
+    parser.add_argument("--num_workers", type=int, default=1)
     parser.add_argument('--stages', type=int, default=2)
     parser.add_argument('--features', type=int, default=128)
     parser.add_argument('--level', type=int, default=4)
@@ -35,8 +115,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if not os.path.exists("Result"):
-        os.mkdir("Result")
+    if not os.path.exists("skeleton"):
+        os.mkdir("skeleton")
+
+    if not os.path.exists(os.path.join("skeleton", args.dataset)):
+        os.mkdir(os.path.join("skeleton", args.dataset))
+        os.mkdir(os.path.join("skeleton", args.dataset, "predict"))
+        os.mkdir(os.path.join("skeleton", args.dataset, "gt"))
+
+    cv2.namedWindow("predict", 0)
+    cv2.namedWindow("ground_truth", 0)
 
     assert os.path.exists('Model'), "Please put the models in ./Model folder"
 
@@ -45,8 +133,8 @@ if __name__ == '__main__':
         "label_size" : args.label_size,
         "kernel_size" : args.kernel_size,
         "sigmoid" : args.sigmoid,
-        "dataset" : "test",
-        "test_only" : True,
+        "dataset" : "val",
+        "test_only" : False,
     }
 
     test_loader_parameters = {
@@ -73,7 +161,6 @@ if __name__ == '__main__':
 
     joints = testset.joint_number
     config = testset.config
-    # threshold = testset.threshold
     threshold = testset.cube_size
 
     test_loader = torch.utils.data.DataLoader(testset, **test_loader_parameters)
@@ -85,25 +172,32 @@ if __name__ == '__main__':
     load_model(model, os.path.join('Model', model_name), eval_mode=True)
     model = model.to(device)
 
-    print("running on test dataset ......")
-    with torch.no_grad(), tqdm(total=len(testset) // args.batch_size + 1) as pbar:
-        pre_uvd = []
-        for batch in iter(test_loader):
-            img, label_img, mask, box_size, com = batch
-            
-            img = img.to(device, non_blocking=True)
-            label_img = label_img.to(device, non_blocking=True)
-            mask = mask.to(device, non_blocking=True)
+    index = 0
+    for batch in iter(test_loader):
+        img, label_img, mask, box_size, com, uvd, heatmaps, depthmaps = batch
+        
+        img = img.to(device, non_blocking=True)
+        label_img = label_img.to(device, non_blocking=True)
+        mask = mask.to(device, non_blocking=True)
 
-            results = model(img, label_img, mask)
+        results = model(img, label_img, mask)
 
-            _heatmaps, _depthmaps, _uvd = results[-1]
+        _heatmaps, _depthmaps, _uvd = results[-1]
 
-            _uvd = _uvd.detach().cpu()
-            img = img.cpu()
+        _uvd = _uvd.detach().cpu().numpy()
+        img = img.cpu().numpy()
+        uvd = uvd.numpy()
 
-            skeleton = draw_skeleton_torch(img[0], _uvd[0], testset.config)
-            skeleton = skeleton.permute(1, 2, 0).numpy()
-            fig, ax = plt.subplots()
-            ax.imshow(skeleton)
-            plt.show()
+        skeleton_gt = draw_skeleton(img[0,0], uvd[0,:,:2])
+        skeleton_pre = draw_skeleton(img[0,0], _uvd[0,:,:2])
+
+        cv2.imshow("predict", skeleton_pre)
+        cv2.imshow("ground_truth", skeleton_gt)
+
+        ch = cv2.waitKey(0)
+        if ch == ord('s'):
+            plt.imsave(os.path.join("skeleton", args.dataset, "predict", "{}.jpg".format(index)), skeleton_pre)
+            plt.imsave(os.path.join("skeleton", args.dataset, "gt", "{}.jpg".format(index)), skeleton_gt)
+            index += 1
+        elif ch == ord('q'):
+            break
